f369cceabbb68b0e8721629ad367d9fb96389d0e
karrk <101902544+karrk@users.noreply.github.com>
2025-05-01 14:00:59 +0900

    [Feat] 데미지 관련 변수, 메서드 추가
    데미지 애니메이션 활용 : Utils 의 정적 메서드를 활용하는 방식으로 구성
    데미지 애니메이션 재생 타이머 : IDamageable 내 타이머 변수 추가

--- Assets/2.Scripts/Interface/IDamageable.cs
+++ Assets/2.Scripts/Interface/IDamageable.cs
@@ -1,5 +1,10 @@
+using UnityEngine;
+
 public interface IDamageable
 {
+    public static Color Color = Color.red;
+    public static float ColorChangeDuration = 0.15f;
+
     /// <summary>
     /// 대상의 체력을 감소합니다. 이후 체력이 없을경우
     /// 사망상태를 전달합니다.
