475eec68a4f5462ecc3d5ec9b19e90b0cfa49499
karrk <101902544+karrk@users.noreply.github.com>
2025-05-01 20:27:00 +0900

    [Feat] HeroController 제작
    몬스터 탐색기를 활용해 몬스터를 찾아
    총의 방향을 결정 후 탄을 발사

--- Assets/2.Scripts/Character/HeroController.cs
+++ Assets/2.Scripts/Character/HeroController.cs
@@ -1,14 +1,32 @@
+using System.Collections;
 using UnityEngine;
 
-public class HeroController : MonoBehaviour, IDamageable
+public class HeroController : InitBehaviour, IDamageable
 {
+    private Gun _gun;
+    private TargetFinder _finder;
     private SpriteRenderer _renderer;
     [SerializeField] private float _hp;
+    [SerializeField] private float _shootingCool;
+    [SerializeField, ReadOnly] private bool _isShootable = true;
+    [SerializeField, ReadOnly] private bool _isUserControlling = false;
     public float HP => _hp;
+    public bool IsDead => _hp <= 0;
 
-    private void Awake()
+    public override int InitOrder => 1;
+
+    protected override void Awake()
     {
+        base.Awake();
+        _gun = GetComponentInChildren<Gun>();
+        _finder = GetComponentInParent<TargetFinder>();
         _renderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void Start()
+    {
+        //_gun.SetBullet(E_Bullet.BasicBullet);
+        //StartCoroutine(StartShooting());
     }
 
     public bool OnDamage(float m_value)
@@ -18,13 +36,57 @@ public class HeroController : MonoBehaviour, IDamageable
         if (_hp <= 0)
         {
             //StartCoroutine(StartDestroy());
-            return true;
         }
         else
         {
             Utils.DamageColorChange(this, _renderer, Color.white);
         }
 
-        return false;
+        return IsDead;
+    }
+
+    /// <summary>
+    /// 쿨타임에 따른 사격을 진행합니다.
+    /// </summary>
+    private IEnumerator StartShooting()
+    {
+        float timer = 0;
+        Vector3 targetPos;
+
+        while (true)
+        {
+            if (IsDead == true)
+                break;
+
+            if(_isShootable == true)
+            {
+                targetPos = _finder.GetTargetPos();
+
+                if(targetPos != Vector3.back) 
+                {
+                    _gun.SetLookTarget(targetPos);
+                    _gun.Fire(E_Bullet.BasicBullet);
+                    timer = _shootingCool;
+                    _isShootable = false;
+                    targetPos = Vector3.back;
+                }
+            }
+            else
+            {
+                timer -= Time.deltaTime;
+            }
+
+
+            if (timer <= 0)
+                _isShootable = true;
+
+            yield return null;
+        }
+    }
+
+    public override void Init()
+    {
+        _gun.SetBullet(E_Bullet.BasicBullet);
+        StartCoroutine(StartShooting());
     }
 }
